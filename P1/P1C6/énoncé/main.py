a7ba896ee63a8303b374de37d306434bacdd573c
MaximeJB <maxym.jb@gmail.com>
2024-12-28 15:45:11 +0100

    Exo fruits

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,9 @@
 # Écrivez votre code ici !
+#création de la liste
+fruits = ["pomme","banane","orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1]= "ananas"
+len(fruits)
+fruits.sort()
+print(fruits)
