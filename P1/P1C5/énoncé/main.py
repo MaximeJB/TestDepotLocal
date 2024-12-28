29a8e736f3ad36cc63503db2b8b6e1a99fabaa8c
MaximeJB <maxym.jb@gmail.com>
2024-12-28 15:25:02 +0100

    Exercice P1C5 refait
    Ils utilisaient le print(f), ils ne savaient pas ce qu'ils faisaient.

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,20 @@
 # Écrivez votre code ici !
+
+
+
+
+nom = "JB"
+age = 28
+taille = 1.83
+est_etudiant = True
+
+print(f"Nom : {nom}")
+print(f"Age : {age}")
+print(f"Taille : {taille}")
+print(f"Est etudiant : {est_etudiant}")
+
+# Afficher les types des quatres variables
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
