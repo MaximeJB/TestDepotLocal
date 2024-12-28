82389a3196e95f53d69e58c7296dd24af85ce825
MaximeJB <maxym.jb@gmail.com>
2024-12-28 14:59:20 +0100

    Many changes
    PLEINS de changements

--- P1/P1C3/correction/main.py
+++ P1/P1C3/correction/main.py
@@ -1,5 +1,1 @@
-# Écrire le message à l'aide de la fonction print()
-print("J'apprends Python !")
-
-#Afficher le résulat d'un calcul
-print(17 + 35 * 2)
+print("hello world")
---
+++ P1/test/Untitled-1.py
@@ -0,0 +1,10 @@
+liste = ["Red", "Bleu", "Vert"]
+for color in liste:
+    print(color)
+    if color == "Red":
+        print("Je suis la couleur rouge")
+    elif color == "Vert":
+        print("je suis vert")
+    else: 
+        print("Je suis une autre couleur que rouge")
+    
