64861547f50a7b17166e5b926a5837b274bbe0a1
MaximeJB <maxym.jb@gmail.com>
2025-01-08 20:42:12 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,9 +1,21 @@
 # Écrivez votre code ici !
 #création de la liste
 fruits = ["pomme","banane","orange"]
+
+#Ajout de kiwi
 fruits.append("kiwi")
+
+#supprésion de orange
 fruits.remove("orange")
+
+#modification du deuxième élément
 fruits[1]= "ananas"
+
+#longueur de la liste
 len(fruits)
+
+#trie par ordre alphabétique
 fruits.sort()
+
+#print
 print(fruits)
