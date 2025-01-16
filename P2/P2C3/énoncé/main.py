9798fddaabf431fe223fbfba0f329a1e60f88cf6
MaximeJB <maxym.jb@gmail.com>
2025-01-16 12:29:00 +0100

    mini change

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -14,7 +14,7 @@ def salaire_horaire(salaire_hebdomadaire, heures_travaillees) :
 data_salaire_annuel=int(input("Saissisez votre salaire annuel :"))
 data_salaire_mensuel= salaire_mensuel(data_salaire_annuel)
 data_salaire_hebdomadaire = salaire_hebdomadaire(data_salaire_mensuel)
-data_heures_travaillees=int(input("Saissiez le nombre d'heures travaillées en semaine"))
+data_heures_travaillees=int(input("Saissiez le nombre d'heures travaillées en semaine : "))
 resultat = salaire_horaire(data_salaire_mensuel, data_heures_travaillees)
 print(f"Votre salaire horaires est de {resultat}")
 
